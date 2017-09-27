ccc7d6465008462fb5c2230e8456a07d000d1cf5
pkhilenko <pkhilenko@yandex.ru>
2017-09-27 19:21:40 +0600

    add resource post

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  validates :title, :body, presence: true
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+
+  def normalize_friendly_id(string)
+    string.to_slug.normalize.to_s
+  end
+
+end
