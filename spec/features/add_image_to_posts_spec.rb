dcc0deee63645c0d0bb4cfb1f031dd2a6be8f59c
pkhilenko <pkhilenko@yandex.ru>
2017-10-02 14:56:35 +0600

    finish spec img

---
+++ spec/features/add_image_to_posts_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "AddImageToPosts", type: :feature do
+  given(:admin) { create :admin }
+
+  scenario 'attach image' do
+    visit new_admin_session_path
+    fill_in 'Электронная почта', with: admin.email
+    fill_in 'Пароль', with: admin.password
+    click_on 'Войти'
+
+    visit admin_posts_path
+    click_on 'Новая'
+    fill_in 'Title', with: 'тестовое название'
+    fill_in 'Body', with: 'Текст статьи'
+    attach_file 'Image', "#{Rails.root}/spec/test2.png"
+    click_on 'Сохранить'
+    click_on 'тестовое название'
+    expect(page).to have_css("img[src*='test2.png']")
+  end
+end
