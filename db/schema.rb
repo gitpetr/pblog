7b584e915eeb2e328c8fcb41db91d003ed90a545
pkhilenko <pkhilenko@yandex.ru>
2017-10-02 13:12:52 +0600

    shema.rb opat

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170927112002) do
+ActiveRecord::Schema.define(version: 20171002023750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -53,7 +51,6 @@ ActiveRecord::Schema.define(version: 20170927112002) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
