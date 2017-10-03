2ca6d36e54323411e73ce14c42504d2d9ad0b325
pkhilenko <pkhilenko@yandex.ru>
2017-10-03 16:58:04 +0600

    ckeditor height

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,6 +1,7 @@
 CKEDITOR.editorConfig = function (config) {
     // ... other configuration ...
-
+    config.language = 'ru';
+    config.height = '1200px';
     config.toolbar_mini = [
         ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
     ];
