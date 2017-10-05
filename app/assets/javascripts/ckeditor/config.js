db0444bbd5941cd16d82455b12fc4da65e9f174d
pkhilenko <pkhilenko@yandex.ru>
2017-10-05 12:20:49 +0600

    delete checkbox from form

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,7 +1,7 @@
 CKEDITOR.editorConfig = function (config) {
     // ... other configuration ...
     config.language = 'ru';
-    config.height = '1200px';
+    config.height = '600px';
     config.toolbar_mini = [
         ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
     ];
