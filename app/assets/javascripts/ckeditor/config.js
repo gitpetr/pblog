0c5c5ddff4603fa428400a0411254be929d46d40
pkhilenko <pkhilenko@yandex.ru>
2017-10-05 14:43:58 +0600

    edit ckeditor config

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,10 +2,34 @@ CKEDITOR.editorConfig = function (config) {
     // ... other configuration ...
     config.language = 'ru';
     config.height = '600px';
-    config.toolbar_mini = [
-        ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
-    ];
-    config.toolbar = "mini";
-
     // ... rest of the original config.js  ...
-}
+    config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+    config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+    config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+    config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+
+    config.toolbar_Pure = [
+        '/', {
+            name: 'basicstyles',
+            items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
+        }, {
+            name: 'paragraph',
+            items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
+        }, {
+            name: 'links',
+            items: ['Link', 'Unlink']
+        }, '/', {
+            name: 'styles',
+            items: ['Styles', 'Format', 'Font', 'FontSize']
+        }, {
+            name: 'colors',
+            items: ['TextColor', 'BGColor']
+        }, {
+            name: 'insert',
+            items: ['Image', 'Table', 'HorizontalRule', 'PageBreak']
+        }
+    ];
+    config.toolbar = 'Pure';
+    return true;
+};
